ebf80433ce5c76ac30c987280eddbdc81abf3e49
Nicholas Leoutsakos <nleoutsa@gmail.com>
2015-03-18 07:26:30 -0400

    trying gem rtf

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,17 +1,6 @@
-require 'htmltoword'
 
 class LettersController < ApplicationController
 
-  # On your controller.
-  respond_to :docx
 
-  # filename and word_template are optional. By default it will name the file as your action and use the default template provided by the gem. The use of the .docx in the filename and word_template is optional.
-  def create
-    # ...
-    @letter = Letter.new
-    # respond_with(@letter, filename: 'my_file.docx', word_template: 'my_template.docx')
-    # Alternatively, if you don't want to create the .docx.erb template you could
-    respond_with(@letter, content: '<html><body>some html</body></html>', filename: 'nmml_file.docx')
-  end
 
 end
