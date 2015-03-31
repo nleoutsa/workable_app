82b69fe6fe2176feeaea1f66d1b26959a0e1cccd
Nicholas Leoutsakos <nleoutsa@gmail.com>
2015-03-31 12:56:09 -0400

    set new heroku config var

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,11 +1,4 @@
 
-if Rails.env.development?
-   Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
-end
-
-if Rails.env.production?
-   Gibbon::API.api_key = ENV["api_key"]
-end
-
+Gibbon::API.api_key = ENV["MAILCHIMP_API_KEY"]
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
